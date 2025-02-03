b0a06970dd3077974c4da8100de9f6c7295a1411
raysonbs <rayson.bs@hotmail.com>
2025-02-02 23:32:51 -0400

    versão_final

--- 1_home.py
+++ 1_home.py
@@ -4,8 +4,8 @@ import pandas as pd
 from datetime import datetime, date
 
 if "data" not in st.session_state:
-    df_data = pd.read_csv(r"C:\Users\RAYSON\3_Streamlit_Game_Dados_Fifa\datasets\CLEAN_FIFA23_official_data.csv",index_col=0)
-    df_data = df_data[df_data['Contract Valid Until'] >= datetime.today().year]
+    df_data = pd.read_csv(r"C:\Users\RAYSON\4_Streamlit_Game_Dados_Fifa\datasets\CLEAN_FIFA23_official_data.csv",index_col=0)
+    df_data = df_data[df_data['Contract Valid Until'] >= 2021]
     df_data = df_data[df_data['Value(£)'] > 0]
     df_data = df_data.sort_values(by="Overall", ascending=False)
     st.session_state["data"] = df_data
